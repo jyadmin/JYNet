7ef576ab2a6880b57eeec5a6ff5106ae14bc2e3f
jyadmin <x289328110@qq.com>
2016-12-07 18:30:06 +0800

    login

--- CheckRepair/Login/Form1.Designer.cs
+++ CheckRepair/Login/Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txButton1 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txButton2 = new TX.Framework.WindowUI.Controls.TXButton();
+            this.txTextBox1 = new TX.Framework.WindowUI.Controls.TXTextBox();
+            this.txTextBox2 = new TX.Framework.WindowUI.Controls.TXTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txButton1
+            // 
+            this.txButton1.Image = null;
+            this.txButton1.Location = new System.Drawing.Point(167, 206);
+            this.txButton1.Name = "txButton1";
+            this.txButton1.Size = new System.Drawing.Size(100, 28);
+            this.txButton1.TabIndex = 0;
+            this.txButton1.Text = "登录";
+            this.txButton1.UseVisualStyleBackColor = true;
+            this.txButton1.Click += new System.EventHandler(this.txButton1_Click);
+            // 
+            // txButton2
+            // 
+            this.txButton2.Image = null;
+            this.txButton2.Location = new System.Drawing.Point(311, 206);
+            this.txButton2.Name = "txButton2";
+            this.txButton2.Size = new System.Drawing.Size(100, 28);
+            this.txButton2.TabIndex = 1;
+            this.txButton2.Text = "取消";
+            this.txButton2.UseVisualStyleBackColor = true;
+            this.txButton2.Click += new System.EventHandler(this.txButton2_Click);
+            // 
+            // txTextBox1
+            // 
+            this.txTextBox1.BackColor = System.Drawing.Color.Transparent;
+            this.txTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(168)))), ((int)(((byte)(192)))));
+            this.txTextBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txTextBox1.HeightLightBolorColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(67)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+            this.txTextBox1.Image = null;
+            this.txTextBox1.ImageSize = new System.Drawing.Size(0, 0);
+            this.txTextBox1.Location = new System.Drawing.Point(242, 111);
+            this.txTextBox1.Name = "txTextBox1";
+            this.txTextBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.txTextBox1.PasswordChar = '\0';
+            this.txTextBox1.Required = false;
+            this.txTextBox1.Size = new System.Drawing.Size(169, 22);
+            this.txTextBox1.TabIndex = 2;
+            // 
+            // txTextBox2
+            // 
+            this.txTextBox2.BackColor = System.Drawing.Color.Transparent;
+            this.txTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(182)))), ((int)(((byte)(168)))), ((int)(((byte)(192)))));
+            this.txTextBox2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txTextBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txTextBox2.HeightLightBolorColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(67)))), ((int)(((byte)(165)))), ((int)(((byte)(220)))));
+            this.txTextBox2.Image = null;
+            this.txTextBox2.ImageSize = new System.Drawing.Size(0, 0);
+            this.txTextBox2.Location = new System.Drawing.Point(242, 157);
+            this.txTextBox2.Name = "txTextBox2";
+            this.txTextBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.txTextBox2.PasswordChar = '*';
+            this.txTextBox2.Required = false;
+            this.txTextBox2.Size = new System.Drawing.Size(169, 22);
+            this.txTextBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(164, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "用户名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(164, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密  码：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(620, 337);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txTextBox2);
+            this.Controls.Add(this.txTextBox1);
+            this.Controls.Add(this.txButton2);
+            this.Controls.Add(this.txButton1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Location = new System.Drawing.Point(300, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "用户登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TX.Framework.WindowUI.Controls.TXButton txButton1;
+        private TX.Framework.WindowUI.Controls.TXButton txButton2;
+        private TX.Framework.WindowUI.Controls.TXTextBox txTextBox1;
+        private TX.Framework.WindowUI.Controls.TXTextBox txTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CheckRepair/Login/Form1.cs
+++ CheckRepair/Login/Form1.cs
@@ -5,17 +5,60 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using TX.Framework.WindowUI.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Login
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MainForm
     {
         public Form1()
         {
+                     
             InitializeComponent();
             //SSS短短的
+            
+        }
+
+        private void txButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txButton1_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            try
+            {
+                string connstr = ConfigurationManager.AppSettings["strCon"];  
+                SqlConnection conn = new SqlConnection(connstr);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                string username = txTextBox1.Text.ToString();
+                string password = txTextBox2.Text.ToString();
+                string selectSql = "select password from C_DMS_User where  loginname = '" + username + "'";
+                cmd.CommandText = selectSql;
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    msg = reader.GetString(0);
+                   
+                }
+                else
+                {
+                    msg = "未查询到数据";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            this.Info(msg);
+
         }
     }
 }
