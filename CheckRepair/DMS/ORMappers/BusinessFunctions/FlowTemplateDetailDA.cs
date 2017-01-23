91c8d77df0b3192e9748ed42d759aaab1f07b934
jyadmin <x289328110@qq.com>
2017-01-23 12:08:43 +0800

    commit

---
+++ CheckRepair/DMS/ORMappers/BusinessFunctions/FlowTemplateDetailDA.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DMS.BaseORMappers.Mapping;
+
+namespace DMS.ORMappers.BusinessFunctions
+{
+    /// <summary>
+    /// 设备接收表
+    /// </summary>
+    [Table("C_DMS_FlowTemplateDetail")]
+    public class FlowTemplateDetailDA : BaseEntity<FlowTemplateDetailDA>
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 关联ID
+        /// </summary>
+        [Column(IsChangeable = true)]
+        public int LinkId
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 操作顺序
+        /// </summary>
+        [Column(IsChangeable = true)]
+        public int OperateOrder
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 作业项点
+        /// </summary>
+        [Column(IsChangeable = true)]
+        public string OperateItem
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 作业内容与技术标准
+        /// </summary>
+        [Column(IsChangeable = true)]
+        public string OperateContentAndStandard
+        {
+            get;
+            set;
+        }
+    }
+}
